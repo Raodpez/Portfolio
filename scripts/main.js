8f21b1e54e3ff1392771bfd2b6a11495d63ce54d
Raciel <98681420+Raodpez@users.noreply.github.com>
2023-12-26 22:47:05 -0500

    almost finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,28 @@
 const menuIcon = document.querySelector('#menu-icon') , menu = document.querySelector('#menu');
+const themeButton = document.querySelector('#themebutton');
 
 menuIcon.addEventListener('click' , (e) => {
     menu.classList.toggle('on');
 
-    const actualRute = e.target.getAttribute('src');
-
-    switch (actualRute) {
-        case './images/open-menu.png':
-            e.target.setAttribute('src' , './images/open-menu2.png');
-            break;
-    
-        default:
-            e.target.setAttribute('src' , './images/open-menu.png');
-            break;
+    if(themeButton.classList.contains('light')){
+        const actualRute = e.target.getAttribute('src');
+        switch (actualRute) {
+            case './images/open-menu.png':
+                e.target.setAttribute('src' , './images/open-menu2.png');
+                break;
+        
+            default:
+                e.target.setAttribute('src' , './images/open-menu.png');
+                break;
+        }
     }
+});
+
+
+themeButton.addEventListener('click' , (e) => {
+    themeButton.classList.toggle('dark');    
+    themeButton.classList.toggle('light');
+
+    document.body.classList.toggle('dark-body');
+    document.body.classList.toggle('light-body');
 });
