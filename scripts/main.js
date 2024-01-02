b7fc17fe4f47ea383466363ad8107c1e29f91f7e
Raciel <98681420+Raodpez@users.noreply.github.com>
2024-01-02 09:08:18 -0500

    Buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector('#menu-icon') , menu = document.querySelector('#menu');
 const themeButton = document.querySelector('#themebutton');
+const userName = document.querySelector('#allbutemail') , userNameLabel = document.querySelector('#allbutemaillabel') , emailBack = document.querySelector('#emailback') , emailBackLabel = document.querySelector('#emailbacklabel'), waySelector = document.querySelector('#way');
 
 menuIcon.addEventListener('click' , (e) => {
     menu.classList.toggle('on');
@@ -18,7 +19,6 @@ menuIcon.addEventListener('click' , (e) => {
     }
 });
 
-
 themeButton.addEventListener('click' , (e) => {
     themeButton.classList.toggle('dark');    
     themeButton.classList.toggle('light');
@@ -26,3 +26,29 @@ themeButton.addEventListener('click' , (e) => {
     document.body.classList.toggle('dark-body');
     document.body.classList.toggle('light-body');
 });
+
+document.body.addEventListener('click' , (e) => {
+    if(waySelector.selectedIndex == 0){
+        userNameLabel.classList.add('hidden');
+        userName.classList.add('hidden');
+    
+        emailBack.classList.remove('hidden');
+        emailBackLabel.classList.remove('hidden');
+    }else{
+        emailBackLabel.classList.add('hidden');
+        emailBack.classList.add('hidden');
+    
+        userName.classList.remove('hidden');
+        userNameLabel.classList.remove('hidden');
+
+        switch(waySelector.selectedIndex){
+            case 3 : 
+                userName.setAttribute('placeholder' , 'ej.@Raodpez')
+                break;
+            default:
+                userName.setAttribute('placeholder' , 'ej.Raodpez')
+                break;
+        }
+    }
+})
+
